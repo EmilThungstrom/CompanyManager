fd06c21aba5b242a42b54edc8089cb7343f4fa8e
EmilThungstrom <45963595+EmilThungstrom@users.noreply.github.com>
2019-04-16 12:11:17 +0200

    Work on DepartmentRepository testing
    Implemented two tests and makes sure that teams and employees removed from a department entity gets removed from the database,
    Changed DepartmentRepository from extending CrudRepository to JpaRepository

--- CompanyManager/src/main/java/se/lexicon/emil/CompanyManager/repositories/DepartmentRepository.java
+++ CompanyManager/src/main/java/se/lexicon/emil/CompanyManager/repositories/DepartmentRepository.java
@@ -1,12 +1,12 @@
 package se.lexicon.emil.CompanyManager.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.emil.CompanyManager.entities.Department;
 import se.lexicon.emil.CompanyManager.entities.Employee;
 
 import java.util.List;
 
-public interface DepartmentRepository extends CrudRepository<Department, Integer> {
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 
     List<Department> findByNameContaining(String name);
 
--- CompanyManager/src/test/java/se/lexicon/emil/CompanyManager/repositoryTests/DepartmentRepositoryTest.java
+++ CompanyManager/src/test/java/se/lexicon/emil/CompanyManager/repositoryTests/DepartmentRepositoryTest.java
@@ -9,13 +9,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import se.lexicon.emil.CompanyManager.entities.Department;
 import se.lexicon.emil.CompanyManager.entities.Employee;
+import se.lexicon.emil.CompanyManager.entities.Team;
 import se.lexicon.emil.CompanyManager.repositories.DepartmentRepository;
 import se.lexicon.emil.CompanyManager.repositories.EmployeeRepository;
+import se.lexicon.emil.CompanyManager.repositories.TeamRepository;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -26,15 +28,19 @@ public class DepartmentRepositoryTest {
     private EmployeeRepository employeeRepository;
     @Autowired
     private DepartmentRepository departmentRepository;
+    @Autowired
+    private TeamRepository teamRepository;
 
     private Department testDepartment;
     private Employee testHead;
+    private Team testTeam;
+    private List<Employee> testEmployees;
 
     @Before
     public void init() {
 
-        Employee employee1 = new Employee("Fist1", "Last1", "adress1", "email1", null, null);
-        Employee employee2 = new Employee("Fist2", "Last2", "adress2", "email2", null, null);
+        Employee employee1 = new Employee("Fist1", "Last1", "adress1", "email1@email.com", null, null);
+        Employee employee2 = new Employee("Fist2", "Last2", "adress2", "email2@email.com", null, null);
 
         testHead = employee1;
 
@@ -49,6 +55,24 @@ public class DepartmentRepositoryTest {
 
         testDepartment = departmentRepository.save(department1);
         departmentRepository.save(department2);
+
+        testHead.setDepartment(department1);
+
+        Team team = new Team(department1, null);
+        Employee employee3 = new Employee("Fist3", "Last3", "adress3", "email3@email.com", null, department1);
+        Employee employee4 = new Employee("Fist4", "Last4", "adress4", "email4@email.com", null, department1);
+        Employee employee5 = new Employee("Fist5", "Last5", "adress5", "email5@email.com", null, department1);
+        Employee employee6 = new Employee("Fist6", "Last6", "adress6", "email6@email.com", null, department1);
+        Employee employee7 = new Employee("Fist7", "Last7", "adress7", "email7@email.com", null, department1);
+
+        testEmployees = (List<Employee>) employeeRepository.saveAll(Arrays.asList(employee1, employee3, employee4, employee5, employee6, employee7));
+        team.setLeader(employee3);
+        testTeam = teamRepository.save(team);
+
+        testDepartment.setEmployees(testEmployees);
+        testDepartment.setTeams(Arrays.asList(testTeam));
+
+        departmentRepository.flush();
     }
 
     @Test
@@ -63,5 +87,23 @@ public class DepartmentRepositoryTest {
         List<Department> expected = Arrays.asList(testDepartment);
         List<Department> actual = departmentRepository.findByHead(testHead);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_RemoveEmployee(){
+        int employeeId = testEmployees.get(3).getId();
+        testDepartment.getEmployees().set(3, null);
+        departmentRepository.flush();
+
+        assertFalse(employeeRepository.findById(employeeId).isPresent());
+    }
+
+    @Test
+    public void test_RemoveTeam(){
+        int teamId = testTeam.getId();
+        testDepartment.getTeams().set(0, null);
+        departmentRepository.flush();
+
+        assertFalse(teamRepository.findById(teamId).isPresent());
     }
 }
