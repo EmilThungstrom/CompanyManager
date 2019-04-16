9b17564e08bd2ef6d2c34b9f6cccee5ac49fb339
EmilThungstrom <45963595+EmilThungstrom@users.noreply.github.com>
2019-04-16 10:10:05 +0200

    Added relevant test to the TODO in EntityGenerationTest

--- CompanyManager/src/test/java/se/lexicon/emil/CompanyManager/testing/EntityGenerationTest.java
+++ CompanyManager/src/test/java/se/lexicon/emil/CompanyManager/testing/EntityGenerationTest.java
@@ -43,7 +43,6 @@ class EntityGenerationTest {
 
     @Test
     void createEmployee() {
-        //TODO: clean up data files used to generate employee fields so it wont generate emails of wrong format
         Department department = new Department("department1", null);
         Team team = new Team(department, null);
         Employee employee = entityGeneration.createEmployee(department, team);
@@ -55,5 +54,11 @@ class EntityGenerationTest {
         assertThat(employee.getLastName().length(), greaterThan(1));
         assertThat(employee.getAddress().length(), greaterThan(1));
         assertThat(employee.getEmail().length(), greaterThan(1));
+        /*
+        TODO: clean files used for generating employee fields so it wont sometimes generate emails that fails validation
+        assertTrue(employee.getEmail().matches(
+                "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        ));
+        */
     }
 }
